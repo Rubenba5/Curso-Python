c4923ee4ff994e8cf131bae8462e2c68f238f9bf
Ruben <rubenblascoarmengod@gmail.com>
2025-04-13 00:13:59 +0200

    Día 9: Módulo Collections

---
+++ Dia 9/General.py
@@ -0,0 +1,10 @@
+# Módulo Collections:
+# El módulo collections proporciona alternativas a los tipos de datos integrados de Python.
+# from collections import *
+
+# Hay varias clases dentro de este módulo, pero las más importantes son:
+# 1. Counter(): sirve para contar elementos en una lista. Lo organiza en un diccionario. Los counter tienen métodos como most_common().
+# 2. defaultdict(lambda: ""): es un diccionario que devuelve un valor por defecto si la clave no existe.
+# 3. namedtuple(): es una tupla con nombre. Permite acceder a los elementos por nombre en lugar de por índice. Se aceede usando, por ejemplo, "tupla.nombre".
+
+# -------------------------------------------------------------------------------------------------------------------- #
