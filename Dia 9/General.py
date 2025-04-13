1d86b5a5549c7250cc1e1a5dc27ea25579606850
Ruben <rubenblascoarmengod@gmail.com>
2025-04-13 10:36:57 +0200

    Día 9: Módulo Collections (deque)

--- Dia 9/General.py
+++ Dia 9/General.py
@@ -6,5 +6,6 @@
 # 1. Counter(): sirve para contar elementos en una lista. Lo organiza en un diccionario. Los counter tienen métodos como most_common().
 # 2. defaultdict(lambda: ""): es un diccionario que devuelve un valor por defecto si la clave no existe.
 # 3. namedtuple(): es una tupla con nombre. Permite acceder a los elementos por nombre en lugar de por índice. Se aceede usando, por ejemplo, "tupla.nombre".
+# 4. deque(): es una lista que permite añadir y quitar elementos por ambos lados. Es más eficiente que una lista normal.
 
 # -------------------------------------------------------------------------------------------------------------------- #
