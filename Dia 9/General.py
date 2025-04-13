98d0d3f2d7e7dc59474be8dcc3be8b8556181eef
Ruben <rubenblascoarmengod@gmail.com>
2025-04-13 10:44:43 +0200

    Día 9: Guardado

--- Dia 9/General.py
+++ Dia 9/General.py
@@ -9,3 +9,21 @@
 # 4. deque(): es una lista que permite añadir y quitar elementos por ambos lados. Es más eficiente que una lista normal.
 
 # -------------------------------------------------------------------------------------------------------------------- #
+# Módulo OS:
+# El módulo os proporciona una forma de interactuar con el sistema operativo.
+# Es bastante util para trabajar con rutas de archivos y directorios.
+
+# Algunas funciones útiles son:
+# 1. os.getcwd(): devuelve el directorio de trabajo actual.
+# 2. os.listdir(): devuelve una lista de los archivos y directorios en un directorio dado.
+# 3. os.unlink(): elimina un archivo o enlace simbólico.
+# 4. os.rmdir(): eliminar un nuevo directorio.
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# Módulo Shutil:
+# El módulo shutil proporciona funciones para trabajar con archivos y directorios.
+# Es bastante útil para copiar, mover y eliminar archivos y directorios.
+
+# Algunas funciones útiles son:
+# 1. shutil.move("archivo", "ruta"): mueve un archivo o directorio a una nueva ubicación.
+# 2. shutil.rmtree("directorio"): elimina un directorio y su contenido de forma recursiva.
