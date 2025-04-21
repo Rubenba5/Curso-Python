41663fddf2a2fff855f73e48e85f3c93f421667d
Ruben <rubenblascoarmengod@gmail.com>
2025-04-21 23:21:37 +0200

    Extras: Recopilacion Proyectos

--- Extras/Recopilacion de Projectos/Recopilacion.py
+++ Extras/Recopilacion de Projectos/Recopilacion.py
@@ -12,6 +12,8 @@ import pygame
 import io
 from pygame import mixer
 import random
+import bs4
+import requests
 
 # Dia 1
 def Nombre_Cerveza():
@@ -714,3 +716,39 @@ def juego():
         pygame.display.update()
 
 
+# Dia 11
+def web_scraping():
+
+    # Url Base de la Página Web
+    url_base = 'https://books.toscrape.com/catalogue/page-{}.html'
+
+    # Lista de Titulos con 4 o 5 estrellas
+    titulos_rating_alto = []
+
+    # Iterar Paginas
+    for pagina in range(1, 51):
+
+        # Crear sopa en cada página
+        url_pagina = url_base.format(pagina)
+
+        # Request a la Pagina
+        resultado = requests.get(url_pagina)
+        sopa = bs4.BeautifulSoup(resultado.text, "lxml")
+
+        # Seleccionar datos de los Libros
+        libros = sopa.select(".product_pod")
+
+        # Iterar en los libros
+        for libro in libros:
+
+            # Verificar que el libro tenga 4 o 5 estrellas
+            if len(libro.select(".star-rating.Four")) != 0 or len(libro.select(".star-rating.Five")) != 0:
+                # Titulo del Libro (variable)
+                titulo_libro = libro.select("a")[1]["title"]
+
+                # Agregar libro a la lista
+                titulos_rating_alto.append(titulo_libro)
+
+    # Ver libros de 4 y 5 estrellas en consola
+    for libro in titulos_rating_alto:
+        print(libro)
