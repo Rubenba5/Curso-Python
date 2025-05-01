1f6bd9511751affa5f27344c3f172d8bdc4eec7d
Ruben <rubenblascoarmengod@gmail.com>
2025-05-01 22:39:50 +0200

    Día 13: General

---
+++ Dia 13/General.py
@@ -0,0 +1,10 @@
+# Librerias
+import pyttsx3 # Texto a Voz
+import speech_recognition as sr # Voz a Texto
+import pywhatkit # Kits de Herramientas
+import yfinance as yf # Consultar la Bolsa
+import pyjokes # Chistes
+import webbrowser # Navegar en Internet
+
+
+# 
